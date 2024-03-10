20fdf061e94fec5c37b360d59c2933d845597c4c
wjohnston7 <157061764+wjohnston7@users.noreply.github.com>
2024-03-10 16:46:35 -0400

    Create enemy and projectile

--- Vidgame Design - 2D Game/Script/CharacterBody2D.gd
+++ Vidgame Design - 2D Game/Script/CharacterBody2D.gd
@@ -3,43 +3,46 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@onready var _animated_sprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+@onready var alive = true
+@export var health = 20
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-
-func _process(delta):
-	if Input.is_action_pressed("Run right"):
-		position.x += 2.5
-		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.play("Run")
-	elif Input.is_action_pressed("Run left"):
-		position.x -= 2.5
-		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("Run")
-	else: $AnimatedSprite2D.play("Idle")
+	if alive:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+# Handle jump.
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+# Get the input direction and handle the movement/deceleration.
+# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("Run left", "Run right")
+		if direction:
+			velocity.x = direction * SPEED
+			$AnimatedSprite2D.play("Run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			$AnimatedSprite2D.play("Idle")
+		move_and_slide()
 	
-#func _process(delta):
-	#if Input.is_action_pressed("Attack"):
-		#$AnimatedSprite2D.play("Side Attack")
-	#else: $AnimatedSprite2D.play("Idle")
+	
+		if velocity.x > 0:
+			$AnimatedSprite2D.flip_h = false
+		elif velocity.x < 0:
+			$AnimatedSprite2D.flip_h = true
+
+
+func damage(amount):
+	health -= amount
+	if health <= 0:
+		health = 0
+		kill()
+	
+
+func kill():
+	$".".position = $"../Spawn".position
+	print ("I'm Dead!!")
+	
 
---
+++ Vidgame Design - 2D Game/Script/Enemy.gd
@@ -0,0 +1,45 @@
+
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const projectile = preload("res://Scenes/projectile.tscn")
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+
+
+#func _on_fire_rate_timeout():
+	#var new_bullet = projectile.instantiate()
+	#new_bullet.position = $"../Marker2D".position
+	#add_child(new_bullet)
+
+
+
+
+
+func _on_fire_rate_timeout():
+	var new_bullet = projectile.instantiate()
+	new_bullet.position = $Marker2D.position
+	add_child(new_bullet)
