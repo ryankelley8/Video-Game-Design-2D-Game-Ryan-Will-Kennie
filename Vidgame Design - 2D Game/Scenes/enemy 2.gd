ce0fae77f1e254f35791576162013f4369b5df97
ryankelley8 <157061728+ryankelley8@users.noreply.github.com>
2024-03-11 11:55:05 -0400

    Create Power Ups

--- Vidgame Design - 2D Game/Scenes/enemy 2.gd
+++ Vidgame Design - 2D Game/Scenes/enemy 2.gd
@@ -2,8 +2,7 @@ extends Node2D
 var player_position = Vector2()
 var speed = 100
 var player_scene = load("res://Player(Will).tscn")
-var player_instance = player_scene.instance()
-var main_character_node = player_instance.get_node("Main_Character")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +11,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	add_child(player_instance)
-	main_character_node.global_position
 	var direction = (player_position - global_position).normalized()
 	global_position += direction * speed * delta
