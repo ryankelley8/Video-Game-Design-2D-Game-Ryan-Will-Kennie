8e777a7ba6054d73d9f4c3faddea87d43c9b4e7d
ryankelley8 <157061728+ryankelley8@users.noreply.github.com>
2024-03-11 11:34:36 -0400

    Edit enemy 2

--- Vidgame Design - 2D Game/Scenes/enemy 2.gd
+++ Vidgame Design - 2D Game/Scenes/enemy 2.gd
@@ -1,6 +1,9 @@
 extends Node2D
 var player_position = Vector2()
 var speed = 100
+var player_scene = load("res://Player(Will).tscn")
+var player_instance = player_scene.instance()
+var main_character_node = player_instance.get_node("Main_Character")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +12,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	player_position = $"Main Character".global_position
+	add_child(player_instance)
+	main_character_node.global_position
 	var direction = (player_position - global_position).normalized()
 	global_position += direction * speed * delta
