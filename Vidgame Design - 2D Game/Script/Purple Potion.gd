551aebd150631d7db3912f977cc48cfd75e9c17f
wjohnston7 <157061764+wjohnston7@users.noreply.github.com>
2024-03-13 17:25:11 -0400

    make potion bottle disappear

--- Vidgame Design - 2D Game/Script/Purple Potion.gd
+++ Vidgame Design - 2D Game/Script/Purple Potion.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-func _on_PurplePotionBottle_body_entered(body: Node) -> void:
-	# Check if the colliding body is the player
-	if body.is_in_group("Player"):
-		$Area2D.hide()
+#func _on_PurplePotionBottle_body_entered(body: Node) -> void:
+	## Check if the colliding body is the player
+	#if body.is_in_group("Player"):
+		#$Area2D.hide()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +13,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Area2D/AnimatedSprite2D.play('Idol')
+
+
+func _on_area_2d_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		$Area2D.hide()
