2a789c90fb2500df905f55b336d1f0cda840d125
wjohnston7 <157061764+wjohnston7@users.noreply.github.com>
2024-03-13 17:21:10 -0400

    Work on enemies
    Trying to make an enemy move back and forth.
    Also some aesthetic changes to the main scene for the purpose of playtesting

--- Vidgame Design - 2D Game/Script/Will's Script/Enemy.gd
+++ Vidgame Design - 2D Game/Script/Will's Script/Enemy.gd
@@ -7,34 +7,31 @@ const JUMP_VELOCITY = -400.0
 const projectile = preload("res://Scenes/Enemy/projectile.tscn")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+var direction = velocity.x
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-
+	move_left()
 	move_and_slide()
-	
-
-
-#func _on_fire_rate_timeout():
-	#var new_bullet = projectile.instantiate()
-	#new_bullet.position = $"../Marker2D".position
-	#add_child(new_bullet)
-
-
-
 
 
 func _on_fire_rate_timeout():
 	var new_bullet = projectile.instantiate()
 	new_bullet.position = $Marker2D.position
 	add_child(new_bullet)
+
+
+
+#func _on_wall_1_body_entered(body):
+	#$".".flip_h = false
+
+
+func move_left():
+	velocity.x = -200
+func move_right():
+	velocity.x = 200
+
+
+#func _on_wall_2_body_entered(body):
+	#$".".flip_h = true
