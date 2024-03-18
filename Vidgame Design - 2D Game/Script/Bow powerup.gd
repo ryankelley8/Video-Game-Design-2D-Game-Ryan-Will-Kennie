b18cebea8e35e5329b68908a1c7949eb21498e97
wjohnston7 <157061764+wjohnston7@users.noreply.github.com>
2024-03-18 15:38:04 -0400

    Code the bow to shoot arrows

--- Vidgame Design - 2D Game/Script/Bow powerup.gd
+++ Vidgame Design - 2D Game/Script/Bow powerup.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-const projectile = preload("res://Scenes/fire_projectile.png")
+const arrow = preload("res://Scenes/arrow.tscn")
+var canAttack = true
+@export var reload_time = 0.7
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,21 +10,34 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Attack"):
+	if Input.is_action_just_pressed("Attack") and canAttack:
 		attack()
+		startReload()
 	else:
 		pass
 
 
 func _on_area_2d_body_entered(body):
-	print(body)
-	reparent(body)
+	if body.is_in_group("Player"):
+		print(body)
+		reparent(body)
+	else:
+		pass
+
 #func attack():
 	#projectile.instantiate()
 	#projectile.position = $Area2D/Marker2D.position
 	#add_child(projectile)
 func attack():
-	#make the projectile its own scene
-	var new_bullet = projectile.instantiate()
-	new_bullet.position = $Area2D/Marker2D.position
-	add_child(new_bullet)
+	var new_arrow = arrow.instantiate()
+	new_arrow.position = $Marker2D.position
+	add_child(new_arrow)
+
+
+func _on_reload_timer_timeout():
+	canAttack = true
+	$"Reload Timer".stop()
+
+func startReload():
+	canAttack = false
+	$"Reload Timer".start(reload_time)
